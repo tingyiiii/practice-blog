2eda44ef4b0e4301b5046c66578d171c880c158b
Ting <0917355950teresa@gmail.com>
2021-02-04 12:31:12 +0800

    crud-new/create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,22 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path, notice: "文章新增成功"
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
