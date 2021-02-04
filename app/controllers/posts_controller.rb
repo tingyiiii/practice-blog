2d02f7288786f567abfe3cbe2bbbcdf7a3313fa3
Ting <0917355950teresa@gmail.com>
2021-02-04 12:34:50 +0800

    crud-show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+  
   def index
     @posts = Post.all
   end
@@ -16,8 +18,15 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
